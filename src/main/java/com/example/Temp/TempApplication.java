44b570d608b2de7b73153e6e99d38f0b778bf720
Katarzyna Norys <katanor457@student.polsl.pl>
2021-06-09 20:56:07 +0200

    Initial commit

---
+++ src/main/java/com/example/Temp/TempApplication.java
@@ -0,0 +1,21 @@
+package com.example.Temp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class TempApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempApplication.class, args);
+	}
+
+	@GetMapping
+	public String get()
+	{
+		return "Hello Jenkins";
+	}
+}
